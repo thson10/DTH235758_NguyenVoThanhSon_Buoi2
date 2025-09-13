83ddd4818955c94130177c8cd05a8a7d9db04474
DTH235758_Nguyễn Võ Thanh Sơn <nvthanhson0801@gmail.com>
2025-09-13 12:09:50 +0700

    Update Cau4+5+6+7+8Chuong2.py

--- Cau4+5+6+7+8Chuong2.py
+++ Cau4+5+6+7+8Chuong2.py
@@ -1,3 +1,7 @@
-Câu 4:
+- Câu 4:
 Python hỗ trợ những kiểu dữ liệu int, float, complex, string, bool, list, tuple, set, byte ...
-Câu 5:
+- Câu 5:
+Có 3 kiểu ghi chú trong python
+- # dùng để ghi chú 1 dòng
+- """........""" hoặc '''.....'''   dùng để ghi chú nhiều dòng
+- Câu 6: 
